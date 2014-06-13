d6aa0c44dcd72ff6e23a7770cbbb3e0f39c92497
Stephen Haywood <stephen@averagesecurityguy.info>
2014-06-13 10:57:53 -0400

    First zero FP vuln scanner.

---
+++ ostrich.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import time
+
+#-----------------------------------------------------------------------------
+# Ostrich.py is the world's first zero false-positive vulnerability scanner.
+# It is guaranteed to not produce false positives so you can rest assured that
+# any vulnerability it finds is the real deal.
+#
+# This is just a joke. Please do not use this.
+#-----------------------------------------------------------------------------
+print "Ostrich Vulnerability Scanner"
+print "-----------------------------"
+print "The world's only zero false-positive scanner."
+print 
+print "Starting vulnerability scan with 1337 checks."
+print "Doing network discovery."
+time.sleep(10)
+print "Found 42 devices."
+print "Scanning."
+time.sleep(30)
+print "Congratulations, no vulnerabilities were found on your network."
